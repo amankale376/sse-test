01ea9a111be098f665f32b5fa3e65f142bdcd73a
amankale376 <amankale@rapidinnovation.dev>
2024-07-08 19:08:45 +0530

    added cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+    app.enableCors({
+    origin: true, // Allow any origin
+    credentials: true, // This option is required if your frontend application needs to send credentials (like cookies or authentication headers)
+  });
   await app.listen(4001);
 }
 bootstrap();
